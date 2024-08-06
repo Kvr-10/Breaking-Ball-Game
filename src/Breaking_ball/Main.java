c0795604bbb0a7be7ceaed5e6a755a45ee9a0803
Chaman <chamansinha9480@gmail.com>
2024-08-07 02:00:52 +0530

    Version 2.0 - Minor Changes

--- src/Breaking_ball/Main.java
+++ src/Breaking_ball/Main.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
        JFrame obj =new JFrame();
+       ImageIcon icon = new ImageIcon(Main.class.getResource("/Icons/ball.png"));
+       obj.setIconImage(icon.getImage());
        Gameplay gameplay = new Gameplay();
        obj.setBounds(300,50,700,600);
        obj.setTitle("Breakout Ball");
